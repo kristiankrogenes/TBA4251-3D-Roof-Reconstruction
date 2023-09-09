b56f9e2b757707364eda027da15379ffe493f92f
Kristian Krøgenes <kristiankrogenes@gmail.com>
2023-09-09 15:21:57 +0200

    Cluster roof segments completed.

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import numpy as np
 import alphashape 
 import geopandas as gpd
 import pandas as pd
+from shapely import Polygon
+from sklearn.cluster import DBSCAN
 
 class Lo2D():
 
@@ -26,7 +28,7 @@ class Lo2D():
                 for file_name in file_names:
                     FILE_PATH = f'{FILES_PATH}/{file_name}'
                     las = laspy.read(FILE_PATH)
-                    data[data_folder][file_name[:-4]] = las
+                    data[data_folder][file_name] = las
         return data 
     
     def get_alpha_shape_polygons(self, roof_segments):
@@ -55,6 +57,61 @@ class Lo2D():
 
             df_roof_polygons = df_roof_polygons.reset_index(drop=True)
         self.df_roof_polygons = df_roof_polygons
+
+    def cluster_roof_polygons(self):
+
+        df_clustered_roof_polygons = pd.DataFrame(columns=['roof_id', 'geometry'])
+        roof_ids = list(set(list(self.df_roof_polygons['roof_id'])))
+
+        for roof_id in roof_ids:
+
+            roof_segments_df = self.df_roof_polygons.loc[self.df_roof_polygons['roof_id'] == roof_id]
+
+            roof_segment_coords = [[coord[:2] for coord in roof_segments_df.iloc[i].geometry.exterior.coords] for i in range(len(roof_segments_df))]
+
+            roof_coords = np.concatenate(roof_segment_coords, axis=0)
+
+            clustering = DBSCAN(eps=1.6, min_samples=2).fit(roof_coords)
+
+            clusters = {}
+            updated_clustering_labels = []
+            for i, label in enumerate(clustering.labels_):
+                if label==-1:
+                    new_label = label*i-1
+                    clusters[new_label] = {i: roof_coords[i]}
+                    updated_clustering_labels.append(new_label)
+                else:
+                    if label in clusters.keys():
+                        clusters[label][i] = roof_coords[i]
+                    else:
+                        clusters[label] = {i: roof_coords[i]}
+                    updated_clustering_labels.append(label)
+
+            mean_cluster_coords = {}
+            for c in clusters:
+                coords = [clusters[c][p] for p in clusters[c]]
+                mean_coords_x = np.sum([coords[i][0] for i in range(len(coords))]) / len(coords)
+                mean_coords_y = np.sum([coords[i][1] for i in range(len(coords))]) / len(coords)
+                # mean_coords_z = np.sum([coords[i][2] for i in range(len(coords))]) / len(coords)
+                mean_cluster_coords[c] = (mean_coords_x, mean_coords_y, 0)
+
+            updated_coords = [mean_cluster_coords[c] for c in updated_clustering_labels]
+
+            inndeling = [len(coords) for coords in roof_segment_coords]
+            updated_segment_coords = []
+            index_memory = 0
+            for i in range(len(inndeling)):
+                updated_segment_coords.append(updated_coords[index_memory:index_memory+inndeling[i]])
+                index_memory += inndeling[i]
+
+            df = gpd.GeoDataFrame({'roof_id': [roof_id for _ in range(len(roof_segments_df))], 'geometry': [Polygon(coords) for coords in updated_segment_coords]})
+            df_clustered_roof_polygons = pd.concat([df_clustered_roof_polygons, df])
+            
+        self.df_clustered_roof_polygons = df_clustered_roof_polygons.reset_index()
+    
+    def fit(self):
+        self.create_roofs_df()
+        self.cluster_roof_polygons()
     
     def plot_data(self, plot_type):
         match plot_type:
@@ -67,7 +124,22 @@ class Lo2D():
                 for id in roof_ids:
                     roof_segments = self.df_roof_polygons.loc[self.df_roof_polygons['roof_id'] == id]
                     polygons = [poly for poly in list(roof_segments['geometry'])]
-                    Plotter.segmented_polygons_2d(polygons)
+                    Plotter.segmented_polygons_2d(id, polygons)
+            case "clustered_segmented_roof_polygons_2d":
+                roof_ids = list(set(list(self.df_clustered_roof_polygons['roof_id'])))
+                for id in roof_ids:
+                    roof_segments = self.df_clustered_roof_polygons.loc[self.df_clustered_roof_polygons['roof_id'] == id]
+                    polygons = [poly for poly in list(roof_segments['geometry'])]
+                    Plotter.segmented_polygons_2d(id, polygons)
+            case "double_segmented_polygons_2d":
+                roof_ids = list(set(list(self.df_clustered_roof_polygons['roof_id'])))
+                for id in roof_ids:
+                    roof_segments1 = self.df_roof_polygons.loc[self.df_roof_polygons['roof_id'] == id]
+                    roof_segments2 = self.df_clustered_roof_polygons.loc[self.df_clustered_roof_polygons['roof_id'] == id]
+                    polygons1 = [poly for poly in list(roof_segments1['geometry'])]
+                    polygons2 = [poly for poly in list(roof_segments2['geometry'])]
+                    Plotter.double_segmented_polygons_2d(id, polygons1, polygons2)
+
 
 
 
@@ -77,8 +149,7 @@ if __name__ == "__main__":
 
     lo2d = Lo2D(DATA_FOLDER_PATH)
 
-    lo2d.create_roofs_df()
-    lo2d.plot_data("pointcloud_scatterplot_3d")
+    lo2d.fit()
 
-
-    
+    # lo2d.plot_data("pointcloud_scatterplot_3d")
+    lo2d.plot_data("double_segmented_polygons_2d")
--- plotter.py
+++ plotter.py
@@ -7,16 +7,48 @@ class Plotter():
         name = "scatterplot_3d"
         fig = plt.figure()
         ax = plt.axes(projection='3d')
-        ax.scatter(x, y, z, c=color, s= size/4)
+        ax.scatter(x, y, z, c=color, s=size/4)
         # if not os.path.exists(f"../images/{name}"):
         #     os.mkdir(f"../images/{name}")
         # plt.savefig(f"../images/{name}/{roof}.png", bbox_inches='tight')
         plt.show()
 
-    def segmented_polygons_2d(polygons):
+    def segmented_polygons_2d(id, polygons):
         name = "segmented_polygons_2d"
         colors = ['b', 'c', 'g', 'k', 'm', 'r', 'w', 'y']
         poly_colors = [colors[i] for i in range(len(polygons))]
         myPoly = gpd.GeoSeries(polygons)
         myPoly.plot(color=poly_colors)
+        plt.title(id)
+        plt.show()
+
+    def double_segmented_polygons_2d(id, roof1, roof2):
+        name = "segmented_polygons_2d"
+        colors = ['b', 'c', 'g', 'k', 'm', 'r', 'w', 'y']
+
+        poly_colors1 = [colors[i] for i in range(len(roof1))]
+        myPoly1 = gpd.GeoSeries(roof1)
+        # myPoly1.plot(color=poly_colors1)
+
+        poly_colors2 = [colors[i] for i in range(len(roof2))]
+        myPoly2 = gpd.GeoSeries(roof2)
+        # myPoly2.plot(color=poly_colors2)
+
+        # plt.title(id)
+        # plt.show()
+
+        # fig, ax = plt.subplots()
+        # myPoly1.plot(color=poly_colors1, ax=ax)
+        # myPoly2.plot(color=poly_colors2, ax=ax)
+        # plt.title(id)
+        # plt.show()
+
+        fig, axes = plt.subplots(1, 2, figsize=(10, 5))  # Create a figure with two subplots
+        myPoly1.plot(color=poly_colors1, ax=axes[0])  # Plot in the first subplot
+        myPoly2.plot(color=poly_colors2, ax=axes[1])  # Plot in the second subplot
+
+        axes[0].set_title("Plot 1")  # Set title for the first subplot
+        axes[1].set_title("Plot 2")  # Set title for the second subplot
+
+        plt.suptitle(id)  # Set a common title for the entire figure
         plt.show()
